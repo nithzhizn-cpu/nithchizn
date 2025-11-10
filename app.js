c19dfaf478a02d205c9203c9ad159ae71e5f0110
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-10 12:02:16 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function sendMessage() {
-  const msg = document.getElementById("message").value;
-  if (window.Telegram.WebApp) {
-    Telegram.WebApp.sendData(msg);
-    alert("Повідомлення відправлено: " + msg);
-  } else {
-    alert("Цей WebApp має працювати лише всередині Telegram.");
-  }
-}
