69b8198e2af985f24b12190d80f4a42b3c8d200e
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-13 18:48:34 +0200

    Remove sendMessage and loadMessages functions

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-async function sendMessage(room, anon_name, message) {
-    await fetch("https://your-server.com/api/messages/", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({room, anon_name, message})
-    });
-}
 
-
-async function loadMessages(room) {
-    const res = await fetch(`https://your-server.com/api/messages/${room}`);
-    const data = await res.json();
-    console.log(data); // відобразити на сторінці
-}
